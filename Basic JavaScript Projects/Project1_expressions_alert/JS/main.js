2bedc1dd733eff1ad12ef2d7fca3891690e0a695
Kelsey Finn <klfinn5@gmail.com>
2021-04-26 01:10:43 -0400

    js assignemnt

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,12 @@
+var A =  ('Daily problem: 4x-7='); //pop up problem
+var B = "Why was 7 afraid of 9?"; //joke
+var C = " Because 7, 8, 9!";
+
+
+var B = B.fontcolor ("blue"); //joke question color
+var C = C.fontcolor ("purple"); //joke answer color
+
+window.alert(A) //pop up
+document.write(B + C) //concatenate variables
+
+
