c84a7f4f5dc5245916f19c6d16f44ce80c0e402e
Kelsey Finn <klfinn5@gmail.com>
2021-05-03 14:28:40 -0400

    operator

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,5 @@
+function myFunction() {                                             //define function and name
+    var sentence = "It's cool";                                     //define variable
+    sentence += " to be inclusive";
+    document.getElementById("operator") .innerHTML = sentence;       //put value into HTML element
+}
