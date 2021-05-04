17e8eaaa6d2afcbdaf465aca1646c5c0543bcf6d
Kelsey Finn <klfinn5@gmail.com>
2021-05-04 10:52:53 -0400

    comments

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -1,4 +1,4 @@
-function addition_Function() {
-    var addition = 2 + 2;
-    document.getElementById("math").innerHTML = "2 + 2 = " + addition;
+function addition_Function() {                                               //define and name function
+    var addition = 2 + 2;                                                    //define variable
+    document.getElementById("math").innerHTML = "2 + 2 = " + addition;       //put value into result in HTMl element
 }
