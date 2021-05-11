a422224a7e08939043d2552f5883f268de35f247
Kelsey Finn <klfinn5@gmail.com>
2021-05-11 09:19:27 -0400

    dictionary

---
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -0,0 +1,11 @@
+function my_dictionary() {
+    var dinner = {
+        appetizer: "blooming onion",
+        entree: "steak",
+        side1: "mashed potatoes",
+        side2: "broccoli",
+        seasoning: "salt, pepper, garlic",
+        dessert: "chocolate lava cake"
+    };
+    document.getElementById("dictionary").innerHTML = dinner;
+}
