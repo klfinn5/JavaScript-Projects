89cbf531d2a876f6acfd031d21a8d301743c8f26
Kelsey Finn <klfinn5@gmail.com>
2021-07-06 12:23:59 -0400

    maybefix

---
+++ Basic JavaScript Projects/Project8_string_methods/JS/main2.js
@@ -0,0 +1,8 @@
+
+function meow() {                                                         // define and name function
+    var part_1 = "You ";                                                  // define variable
+    var part_2 = "Barium";
+    var sentence = part_1.concat(part_2);                                 // connect strings
+    document.getElementById("conkittycat").innterHTML = sentence;         // define html element
+}
+
