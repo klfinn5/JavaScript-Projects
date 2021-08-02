bce391686d7c1534ca2f2e3c36483d22fb923626
Kelsey Finn <klfinn5@gmail.com>
2021-08-02 12:51:54 -0400

    loop

---
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -0,0 +1,9 @@
+function count() {
+    var Digit = "";
+    var X = 1;
+    while (X < 11) {
+        Digit += "<br>" + X;
+        X++;
+    }
+    document.getElementById("100count").innerHTML = Digit;
+}
